9b22ee83b4a6846b8e76b576ca77ca0cfe340a13
amit-rathore0 <amitrathore.aaa@gmail.com>
2023-05-18 12:31:08 +0530

    shop

--- src/Shop.js
+++ src/Shop.js
@@ -1,8 +1,22 @@
 import React from "react"
+import "./Navigation.css"
 
-const Shop = () => {
+const Shop = (props) => {
+    const cartfunc = () =>{
+        props.setCartstatus(true)
+        props.setCartnumber(props.cartnumber+1)
+    }
     return(
-        <div className="left"><h1>This is the shop</h1></div>
+        <div className="left">
+            <h1>This is the shop</h1>
+                <div className="items">            
+                <div className="item" onClick={()=> cartfunc()}>1</div>
+                <div className="item" onClick={()=> cartfunc()}>2</div>
+                <div className="item" onClick={()=> cartfunc()}>3</div>
+                <div className="item" onClick={()=> cartfunc()}>4</div>
+                <div className="item" onClick={()=> cartfunc()}>5</div>
+            </div>
+        </div>
     )
 }
 
